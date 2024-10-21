17aefbbce791fc69e891133cd20715228d69041c
asmae <asmae.chadli20@gmail.com>
2024-10-21 12:17:18 +0100

    table utilisateur

--- init.sql
+++ init.sql
@@ -6,3 +6,14 @@ CREATE TABLE laboratoire (
     active BOOLEAN DEFAULT true,           
     dateActivation DATE                   
 );
+CREATE TABLE utilisateur (
+    id SERIAL PRIMARY KEY,                             -- Identifiant unique de l'utilisateur
+    email VARCHAR(255) NOT NULL UNIQUE,                -- Adresse email unique pour chaque utilisateur
+    fkIdLaboratoire INT REFERENCES laboratoire(id),    -- Clé étrangère vers la table laboratoire
+    nomComplet VARCHAR(255) NOT NULL,                  -- Nom complet de l'utilisateur
+    profession VARCHAR(100),                           -- Profession ou rôle de l'utilisateur dans le laboratoire
+    numTel VARCHAR(20),                                -- Numéro de téléphone de l'utilisateur
+    signature BYTEA,                                   -- Signature numérique ou image
+    role VARCHAR(50) NOT NULL                          -- Rôle de l'utilisateur (administrateur, technicien, etc.)
+);
+
